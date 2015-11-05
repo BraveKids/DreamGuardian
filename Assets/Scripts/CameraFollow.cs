31f1784b2235894dc9af9a7b185790f366d18bf3
Andrea <andrea.ceccarelli29292@gmail.com>
2015-11-05 16:27:57 +0100

    Camera follow player: first implementation

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class CameraFollow : MonoBehaviour {
+
+    private Vector2 velocity;
+
+    public float smoothTimeY;
+    public float smoothTimeX;
+
+    public GameObject player;
+    public GameObject terrain;
+
+    public Vector3 origin;
+
+
+
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+        origin = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+
+        transform.position = origin;
+
+    }
+
+    void FixedUpdate() {
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        float destY = origin.y;
+        if (player.transform.position.y > origin.y) {destY = player.transform.position.y; }
+        float posY = Mathf.SmoothDamp(transform.position.y, destY, ref velocity.y, smoothTimeY);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+
+    }
+
+}
+
+
