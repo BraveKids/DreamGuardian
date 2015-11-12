3ddc7c2afc9707dad86b1a071810b91f15010a20
Nico <Nico>
2015-11-12 16:58:14 +0100

    Zombie

---
+++ Assets/Scripts/ChaseZombieCone.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChaseZombieCone : MonoBehaviour {
+
+    public bool isLeft = false;
+    public ZombieAI zombieAI;
+
+    void Start()
+    {
+        zombieAI = gameObject.GetComponentInParent<ZombieAI>();
+    }
+    void Update()
+    {
+
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            if (isLeft)
+            {
+                zombieAI.chaseLeft();
+            }
+            else
+            {
+                zombieAI.chaseRight();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ZombieAI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieAI : MonoBehaviour {
+
+    public Transform Player;
+    public float MoveSpeed = 4;
+    public bool facingRight = false;
+
+   /* void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+        if (move < 0 && !facingRight)
+            Flip();
+        else if (move > 0 && facingRight)
+            Flip();
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    } */
+
+    public void chaseLeft()
+    {
+        transform.position -= transform.right * MoveSpeed * Time.deltaTime;
+    }
+    public void chaseRight()
+    {
+        transform.position += transform.right * MoveSpeed * Time.deltaTime;
+    }
+}
