4874c28e3b43c1631b69b348f20e3fde83c1a902
andrea29292 <andrea.ceccarelli29292@gmail.com>
2015-12-02 20:15:13 +0100

    still working on camera, need a fix

---
+++ Assets/Scripts/Camera/movePlatOnCollide.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movePlatOnCollide : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D other) {
+
+			MovingPlatfrom.CAN_MOVE = true;
+				
+	}
+
+}
