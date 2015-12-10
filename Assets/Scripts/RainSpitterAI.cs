0471aba08af8c2fe7506a38fef51341856568bdb
nico <nico_loca_92@hotmail.it>
2015-12-10 12:59:01 +0100

    inizio rain spitter

---
+++ Assets/Scripts/RainSpitterAI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RainSpitterAI : MonoBehaviour {
+
+    
+    public GameObject cannonball;  
+    float bulletTimer;
+    public float shootInterval;
+    public Transform shootPoint;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bulletTimer += Time.deltaTime;
+        if (bulletTimer >= shootInterval)
+        {
+            GameObject ball;
+            ball = Instantiate(cannonball, shootPoint.transform.position, Quaternion.identity) as GameObject;
+            bulletTimer = 0;
+        }
+
+    }
+}
