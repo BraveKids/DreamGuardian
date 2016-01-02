1071949ab7e92d289a8353284125c89c55310d3e
Tommaso Celata <t.celata@gmail.com>
2016-01-02 18:09:26 +0100

    Gorilla Boss script working

--- Assets/Scripts/GorillaBossScript.cs
+++ Assets/Scripts/GorillaBossScript.cs
@@ -5,32 +5,64 @@ public class GorillaBossScript : MonoBehaviour {
 	Rigidbody2D rb;
 	Animator anim;
 	bool vulnerable;
+	bool movingBack;
 	public GameObject enemy;
 	public GameObject[] ropes;
+	public float moveSpeed;
 	int actualRope;
+	int hp;
+	public int hpDelta;
 	// Use this for initialization
 	void Start () {
+		actualRope = 0;
 		rb = GetComponent<Rigidbody2D>();
 		rb.isKinematic = true;
 		vulnerable = false;
+		hp = 12;
+		hpDelta = 0;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(ropes[actualRope].activeSelf == false){
+		Debug.Log (hpDelta);
+
+		if(!ropes[actualRope].activeSelf==true){
 			rb.isKinematic = false;
 			vulnerable = true;
-			actualRope++;
-	
+			actualRope+=1;
+		
+
+			
+
 	}
+		if (hpDelta == 4) {
+			BackToRope();
+			}
+		if (movingBack == true) {
+			gameObject.transform.position = Vector3.MoveTowards (transform.position,ropes [actualRope].transform.position,  Time.deltaTime * moveSpeed);
+
+		}
+		if (transform.position.Equals(ropes [actualRope].transform.position)) {
+			movingBack = false;
+		}
 }
 
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.CompareTag("AttackTrigger") && vulnerable == true){
+			hp-=1;
+			hpDelta+=1;
+			if(hp<=0){
 			enemy.gameObject.SetActive(false);
+			}
+		}
+	}
 
 
-}
+	void BackToRope(){
+		hpDelta=0;
+		vulnerable = false;
+		rb.isKinematic = true;
+		movingBack = true;
 	}
 }
