1d0892da49ff975e8dea1a864888ddf8b842b5ba
andrea29292 <andrea.ceccarelli29292@gmail.com>
2016-01-02 12:27:26 +0100

    started implementing dialog with dream master

--- Assets/Scripts/NewAbility.cs
+++ Assets/Scripts/NewAbility.cs
@@ -7,5 +7,7 @@ public class NewAbility : MonoBehaviour {
 	void OnTriggerEnter2D (Collider2D other) {
 		if (other.CompareTag("Player"))
 			SaveLoad.savedGame.skills.Add(ability);
+
+		//implementare parte che blocca yume durante la spiegazione dell'abilità
 	}
 }
