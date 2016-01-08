ab377af13d68c4fff5c920d89fc86d138ed85e47
andrea29292 <andrea.ceccarelli29292@gmail.com>
2016-01-08 20:16:29 +0100

    done

--- Assets/Scripts/SavingSystem/NextLevel.cs
+++ Assets/Scripts/SavingSystem/NextLevel.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class NextLevel : MonoBehaviour {
 
-	public GameObject nextLevel;
-	public GameObject savePoint;
+	public GameObject levelStart;
+
 	// Use this for initialization
 	void OnTriggerEnter2D (Collider2D other) {
 		
 		if (other.CompareTag ("Player")) {
+			GameObject nextLevel = levelStart.transform.FindChild("reachLevel").gameObject;
+			GameObject savePoint = levelStart.transform.FindChild("SavePoint").gameObject;
+			
 			SaveLoad.savedGame.x = nextLevel.transform.position.x;
 			SaveLoad.savedGame.y = nextLevel.transform.position.y;
 			Debug.Log(nextLevel.transform.position.x-transform.position.x);
