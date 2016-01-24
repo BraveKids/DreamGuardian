9849b12bdfe26abc5101b3df5a112fedacca9328
Tommaso Celata <t.celata@gmail.com>
2016-01-24 19:49:03 +0100

    change night animation and hud

--- Assets/Scripts/Misc/nextNight.cs
+++ Assets/Scripts/Misc/nextNight.cs
@@ -5,32 +5,41 @@ public class nextNight : MonoBehaviour {
 
 	public GameObject nightStart;
 	public int level;
+	GameObject player;
 
 	void OnTriggerEnter2D (Collider2D other) {
 		if (other.CompareTag ("Player")) {
+			player = other.gameObject;
 			other.gameObject.GetComponent<CharacterControllerScript> ().stopYume();
+			other.gameObject.GetComponent<Animator>().Play("flare");
 			CameraFollowOnPlatform.instance.setFollowYume (false);
+			Invoke ("Teleport", 2.1f);
 
-			//Animazione con sabbia
-			//System.Threading.Thread.Sleep (2000);
-			//"teletrasporto" di yume
-
-			if (level != null) {
-				SaveLoad.setLevel (level);
-			}
-				
-			SaveLoad.savedGame.x = nightStart.transform.position.x;
-			SaveLoad.savedGame.y = nightStart.transform.position.y;
-				
-			SaveLoad.Spawn ();	
-			//fai partire animazione per la ricomposizione
-
-			CameraFollowOnPlatform.instance.onNewLevel (nightStart.transform.position);
-			other.gameObject.GetComponent<CharacterControllerScript> ().goYume();
-			
 				
 				
 		}		
-	}	
+	}
+
+	void Teleport(){
+		//Animazione con sabbia
+		//System.Threading.Thread.Sleep (2000);
+		//"teletrasporto" di yume
+		
+		if (level != null) {
+			SaveLoad.setLevel (level);
+		}
+		
+		SaveLoad.savedGame.x = nightStart.transform.position.x;
+		SaveLoad.savedGame.y = nightStart.transform.position.y;
+		
+		SaveLoad.Spawn ();	
+		//fai partire animazione per la ricomposizione
+		
+		CameraFollowOnPlatform.instance.onNewLevel (nightStart.transform.position);
+		player.gameObject.GetComponent<Animator> ().Play ("respawn");
+		player.gameObject.GetComponent<CharacterControllerScript> ().goYume();
+
+	}
+	
 }
 
