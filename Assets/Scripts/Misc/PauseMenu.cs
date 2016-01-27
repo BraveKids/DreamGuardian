6c72cb99b8ac7b9d1ffb13e770df51ce565d838a
Tommaso Celata <t.celata@gmail.com>
2016-01-27 18:11:17 +0100

    almost there

--- Assets/Scripts/Misc/PauseMenu.cs
+++ Assets/Scripts/Misc/PauseMenu.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class PauseMenu : MonoBehaviour {
-
+	public GameObject player;
 	// Use this for initialization
 	public void Start () {
 		//showPauseMenu ();
@@ -16,12 +16,13 @@ public class PauseMenu : MonoBehaviour {
 	
 	public void showPauseMenu () {
 		Time.timeScale = 0;
-		
+		player.gameObject.GetComponent<CharacterControllerScript> ().stopYume();
 		gameObject.SetActive (true);
 	}
 	
 	public void hidePauseMenu () {
 		gameObject.SetActive (false);
+		player.gameObject.GetComponent<CharacterControllerScript> ().goYume();
 		Time.timeScale = 1;		
 	}
 	
